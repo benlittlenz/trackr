b08bada5d9713b5b62d2ee93de8060545a828259
nzbenji <benlittle53@gmail.com>
2019-07-31 17:34:43 +1200

    added files and firebase starter

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOrDc6bjiTeavJNHWL5AUkX6lHAxvGj7I",
+  authDomain: "trackr-7c8d9.firebaseapp.com",
+  databaseURL: "https://trackr-7c8d9.firebaseio.com",
+  projectId: "trackr-7c8d9",
+  storageBucket: "",
+  messagingSenderId: "81317051495",
+  appId: "1:81317051495:web:171d8acb40d7fb3d"
+};
+
+  export default firebaseConfig;
